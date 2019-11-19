40f5b2653c46fc426dc1d61e7cba599f75d6186d
James Pierce <james@jpegjames.com>
2019-11-19 05:31:39 -0600

    Disabling files for Heroku

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,16 +1,19 @@
 class Applicant < ActiveRecord::Base
   has_many :member_responses
   has_many :members, :through => :member_responses
-  has_many :files, :class_name => 'ApplicantFile'
-  has_many :applicant_files
   has_one :skype_interview_recording
   belongs_to :position
-  
-  attr_accessible :date_applied, :fname, :lname, :applicant_files_attributes, :skype_date, :position_id
-  accepts_nested_attributes_for :applicant_files
+
+  attr_accessible :date_applied, :fname, :lname, :skype_date, :position_id
+
+  ## Disabling files for Heroku
+  # has_many :files, :class_name => 'ApplicantFile'
+  # has_many :applicant_files
+  # attr_accessible :applicant_files_attributes
+  # accepts_nested_attributes_for :applicant_files
 
   validates_presence_of :fname, :lname, :position_id
-  
+
   def to_s
     self.name
   end
